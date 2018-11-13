01345a6a657264b72bf475f1683e5c24a55e1c43
davidfrichhansen <david.frich.hansen@gmail.com>
2018-11-13 15:40:56 +0100

    added data

--- nmf_nuts.py
+++ nmf_nuts.py
@@ -2,10 +2,14 @@ import numpy as np
 from nuts_goodgrad import NUTS
 import aux_funcs as fs
 from scipy.special import erf, erfinv
-
+from scipy.io import loadmat
 
 
 if __name__ == "__main__":
-    pass
+    # load data
+    mats = loadmat("data/sampling.mat")
+    # data matrix
+    X = mats['X']
+    
     
 
