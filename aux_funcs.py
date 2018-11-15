Something is not right with likelihood. Sometimes dual averaging doesn't converge. Here it probably has wrong sign

aux_funcs.py
@@ -62,10 +62,10 @@ def calcDistanceMatrixFastEuclidean(points):
     distMat = np.sqrt(np.sum((repmat(points, numPoints, 1) - repeat(points, numPoints, axis=0))**2, axis=1))
     return distMat.reshape((numPoints,numPoints))
 
-########################3
+########################
 #### Log likelihood ####
 ########################
-@jit(nopython=False)
+#@jit(nopython=False)
 def loglik(etadelta, sigma, X, linkD, linkH, M, cholD, cholH, *linkArgs):
     # linkD and linkH should be callable and return f^-1(h) and f^-1(d) resp.
     # furthermore it should return the derivative of these!
@@ -75,6 +75,7 @@ def loglik(etadelta, sigma, X, linkD, linkH, M, cholD, cholH, *linkArgs):
     K, L = X.shape
     eta = etadelta[:M*L]
     delta = etadelta[M*L:]
+
 
 
     # convert 'greeks' to matrices D and H - eq. (17)
@@ -98,6 +99,7 @@ def loglik(etadelta, sigma, X, linkD, linkH, M, cholD, cholH, *linkArgs):
     inner2 = ((H @ (X_re - X).T) * D_prime.reshape(M, K))
     grad2 = sigma ** (-2) * (inner2 @ cholD).ravel() + delta
     grad = np.concatenate((grad1, grad2), axis=0)
+    #print(grad.shape)
     return cost_val, grad
 
 

nmf_nuts.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     t0 = time.time()
     print("Loading data")
     # load data
+    """
     file_name1 = '50x50_nw250_noise12_nhs2_k4_21'
     mat1 = loadmat('./data/'+file_name1+'.mat')
     X1 = mat1["X"]
@@ -24,16 +25,18 @@ if __name__ == "__main__":
     X = X1 + X2
     # delete objects from memory
     del mat1, mat2
-
+    """
+    mat = loadmat('./data/sampling.mat')
+    X = mat['X']
     K, L = X.shape
     print("Shape of data is (%d, %d)" % (K,L))
     
     # setup covariances
     print("\n\nSetting up covariances")
-    M = 3
-    beta_H = 10
-    beta_D = 10
-    sigma_N = 2
+    M = 2
+    beta_H = 2.5
+    beta_D = 2.5
+    sigma_N = 5
     
     dim = int(np.sqrt(len(X)))
     cov_D_2d = fs.get_2d_exp_kernel(beta_D, (dim,dim))
@@ -61,16 +64,22 @@ if __name__ == "__main__":
     cholH = np.linalg.cholesky(cov_H)
     # 'wrapper' - may be inefficient
     def loglik_w(etadelta):
-        return loglik(etadelta, sigma_N, X, fs.link_rectgauss, fs.link_exp_to_gauss,M,cholD, cholH,
-                      (np.diag(cov_D_2d), 1), (np.diag(cov_H), 1))
+        return list(map(lambda x : -x, loglik(etadelta, sigma_N, X, fs.link_rectgauss, fs.link_exp_to_gauss,M,cholD, cholH,
+                      (np.diag(cov_D_2d), 1), (np.diag(cov_H), 1))))
                       
     print("Done with additional setup after %f s!\n" % (time.time() - t0))
     
     
-    num_samples = 100
-    num_burnin = int(0.1*num_samples)
+    num_samples = 5000
+    num_burnin = int(0.2*num_samples)
+    #num_burnin = 4900
     print("Setting up sampler wíth %d samples and %d adaptive\n" % (num_samples, num_burnin))
-    sampler = NUTS(loglik_w, num_samples, num_burnin, etadelta)
+    sampler = NUTS(loglik_w, num_samples, num_burnin, etadelta, delta=0.8)
     print("Beginning sampling")
     sampler.sample()
     print("Done sampling after %f s!\n" % (time.time() - t0))
+    out_filename = input("Please input filename for saving\n")
+    out_path = out_filename
+    print("\nSaving into %s" % out_path)
+    np.save(out_filename, sampler.samples)
+    print("Finished in %f s!" % (time.time() - t0))

nuts_goodgrad.py
@@ -78,7 +78,6 @@ class NUTS:
         a = 1.0 if criteria else -1.0
 
         while a*logp_ratio > -a*np.log(2.0):
-            print("hej a %.2f" % a)
             epsilon *= 2.0**a
             _, r_prime, _, new_logp = self.leapfrog(theta, r, epsilon, old_grad)
             logp_ratio = new_logp - old_logp - 0.5*r_prime.T@r_prime + 0.5*r.T@r
@@ -140,7 +139,7 @@ class NUTS:
                 if bern:
                     theta_prime = theta_pp
                     grad_prime = grad_pp
-
+                    
                 alpha_prime += alpha_pp
                 n_alpha += n_alphapp
 
@@ -207,6 +206,7 @@ class NUTS:
                     #bern = np.random.binomial(1, min(1, n_prime / n))
                     bern = np.random.rand() < min(1, n_prime / n)
                     if bern:
+                        #print("Accept!")
                         theta_prop = theta_prime
                         grad = grad_prime
 
@@ -217,12 +217,16 @@ class NUTS:
             # dual averaging
             if m <= M_adapt:
                 H_bar = (1 - 1.0 / (m + t0))*H_bar + 1 / (m+t0) * (self.delta - alpha / n_alpha)
+                #print(alpha / n_alpha)
+                #print(H_bar)
                 logeps = mu - np.sqrt(m) / gamma * H_bar
+                #print(mu)
                 epsilon = np.exp(logeps)
                 logeps_bar = m**(-kappa) * logeps + (1-m**(-kappa))*logeps_bar
+                if m % 100 == 0:
+                    print("epsilon = %f" % epsilon)
             else:
                 epsilon = np.exp(logeps_bar)
-
 
             if self.debug:
                 print(epsilon)